4b1552a7288f434536007e8bc9401fdad577622a
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2024-04-03 11:19:26 +0900

    [#5] SecurityConfig: filterChain 메소드 수정

--- src/main/java/skhu/jijijig/config/SecurityConfig.java
+++ src/main/java/skhu/jijijig/config/SecurityConfig.java
@@ -4,11 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -35,27 +37,26 @@ public class SecurityConfig {
             "/api/member/login",
             "/api/member/logout",
             "/api/member/refresh",
-            "/api/crawling",
+            "/api/crawling/**",
             "/v3/api-docs/**",
             "/swagger-ui/**"
     };
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .formLogin(AbstractHttpConfigurer::disable)
-                .logout(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .requestMatchers(PERMITTED_URLS).permitAll()
-                        .requestMatchers("/api/**").hasAnyAuthority("USER", "ADMIN")
-                        .anyRequest().authenticated()
+        http
+                .cors(Customizer.withDefaults()) // CORS를 기본 설정으로 활성화합니다.
+                .csrf(CsrfConfigurer::disable) // CSRF 보호를 비활성화합니다.
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션을 사용하지 않습니다.
+                .formLogin(Customizer.withDefaults()) // 폼 기반 로그인을 기본 설정으로 활성화합니다.
+                .logout(LogoutConfigurer::permitAll) // 로그아웃을 허용합니다.
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PERMITTED_URLS).permitAll() // 특정 경로에 대한 접근을 모두 허용합니다.
+                        .anyRequest().authenticated() // 그 외 요청은 인증이 필요합니다.
                 )
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .addFilterBefore(new JwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class)
-                .build();
+                .addFilterBefore(new JwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class); // JWT 필터를 추가합니다.
+        return http.build();
     }
 
     @Bean
