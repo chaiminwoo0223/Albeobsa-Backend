202ea83ee4d1d91cbe67d401307130b32e85c8c6
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2024-04-10 12:06:19 +0900

    [#7] Delete src/main/java/skhu/jijijig/domain/repository directory

--- src/main/java/skhu/jijijig/domain/repository/CrawlingRepository.java
+++
@@ -1,9 +0,0 @@
-package skhu.jijijig.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import skhu.jijijig.domain.model.Crawling;
-
-@Repository
-public interface CrawlingRepository extends JpaRepository<Crawling, Long> {
-}
