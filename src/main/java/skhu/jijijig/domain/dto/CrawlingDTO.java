eba607ea22d1324287cdd7f5f5328868f2f32c86
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2024-04-10 11:51:12 +0900

    [#5] Crawling, CrawlingDTO 코드 수정

--- src/main/java/skhu/jijijig/domain/dto/CrawlingDTO.java
+++ src/main/java/skhu/jijijig/domain/dto/CrawlingDTO.java
@@ -3,6 +3,8 @@ package skhu.jijijig.domain.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Builder
 @Getter
 @NoArgsConstructor
@@ -10,4 +12,28 @@ import lombok.*;
 public class CrawlingDTO {
     @Schema(description = "텍스트 데이터", example = "지지직")
     private String text;
+
+    @Schema(description = "제목", example = "비타민 A, B, C, D, E, F")
+    private String title;
+
+    @Schema(description = "이름", example = "chaiminwoo0223")
+    private String name;
+
+    @Schema(description = "이미지", example = "image URL")
+    private String image;
+
+    @Schema(description = "링크", example = "https://link.com")
+    private String link;
+
+    @Schema(description = "등록일", example = "2024-03-30 15:00:00")
+    private String createdDateTime;
+
+    @Schema(description = "조회수", example = "10")
+    private Integer view;
+
+    @Schema(description = "추천수", example = "10")
+    private Integer recommendCnt;
+
+    @Schema(description = "댓글수", example = "10")
+    private Integer commentCnt;
 }
--- src/main/java/skhu/jijijig/domain/model/Crawling.java
+++ src/main/java/skhu/jijijig/domain/model/Crawling.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.*;
 import skhu.jijijig.domain.dto.CrawlingDTO;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Builder
 @Getter
@@ -16,4 +18,28 @@ public class Crawling {
 
     @Column(nullable = false, columnDefinition = "TEXT")
     private String text; // 크롤링한 텍스트 데이터를 저장
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Column(nullable = false)
+    private String link;
+
+    @Column(nullable = false)
+    private String createdDateTime;
+
+    @Column(nullable = false)
+    private Integer view;
+
+    @Column(nullable = false)
+    private Integer recommendCnt;
+
+    @Column(nullable = false)
+    private Integer commentCnt;
 }
