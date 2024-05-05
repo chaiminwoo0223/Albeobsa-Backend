4a50daa7cb9bb033338fb96cfe9fef7287cdb705
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2024-05-05 23:52:31 +0900

    [#4] Delete EmailAlreadyExistsException.java

--- src/main/java/skhu/jijijig/exception/EmailAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package skhu.jijijig.exception;
-
-public class EmailAlreadyExistsException extends RuntimeException {
-    public EmailAlreadyExistsException(String email) {
-        super("이메일이 이미 사용 중입니다: " + email);
-    }
-}
