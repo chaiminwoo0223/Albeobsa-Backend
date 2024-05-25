0353cccb160997c2254c16188697fd453d7b3385
최민우 <chaiminwoo0223@naver.com>
2024-05-25 23:03:19 +0900

    [#15] MemberController: 콜백 기능 추가
    Google OAuth 인증 코드를 받아서, 리다이렉트 URL을 처리합니다.

--- src/main/java/skhu/jijijig/controller/MemberController.java
+++ src/main/java/skhu/jijijig/controller/MemberController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 import skhu.jijijig.domain.dto.ErrorResponseDTO;
 import skhu.jijijig.domain.dto.TokenDTO;
 import skhu.jijijig.service.MemberService;
+
+import java.io.IOException;
 
 @Tag(name = "Member API", description = "사용자 관련 API")
 @RestController
@@ -22,23 +25,38 @@ public class MemberController {
     private final MemberService memberService;
 
     @Value("${spring.security.oauth2.google-authentication-url}")
-    private String authentication_url;
+    private String authenticationUrl;
+
+    @Value("${spring.security.oauth2.google-callback-url}")
+    private String callbackUrl;
 
     @Operation(summary = "인증", description = "Google OAuth 인증 URL을 전달합니다.")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "인증 성공", content = @Content(schema = @Schema(implementation = String.class))),
+            @ApiResponse(responseCode = "200", description = "인증 성공"),
             @ApiResponse(responseCode = "400", description = "잘못된 요청", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class))),
             @ApiResponse(responseCode = "401", description = "인증 실패", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class))),
             @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
     })
     @GetMapping("/authenticate")
     public ResponseEntity<String> authenticate() {
-        return ResponseEntity.ok(authentication_url);
+        return ResponseEntity.ok(authenticationUrl);
+    }
+
+    @Operation(summary = "콜백", description = "Google OAuth 인증 코드를 받아서, 리다이렉트 URL을 처리합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "콜백 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class))),
+            @ApiResponse(responseCode = "401", description = "인증 실패", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class))),
+            @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
+    })
+    @GetMapping("/callback")
+    public void callback(@RequestParam("code") String code, HttpServletResponse response) throws IOException {
+        response.sendRedirect(callbackUrl + code);
     }
 
     @Operation(summary = "로그인", description = "Google OAuth 인증 코드를 사용하여, 사용자 로그인을 처리하고 토큰을 반환합니다.")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "로그인 성공", content = @Content(schema = @Schema(implementation = TokenDTO.class))),
+            @ApiResponse(responseCode = "200", description = "로그인 성공"),
             @ApiResponse(responseCode = "400", description = "잘못된 요청", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class))),
             @ApiResponse(responseCode = "401", description = "인증 실패", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class))),
             @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
@@ -51,9 +69,9 @@ public class MemberController {
 
     @Operation(summary = "리프레시", description = "리프레시 토큰을 사용하여, 액세스 토큰을 갱신합니다.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "리프레시 성공", content = @Content(schema = @Schema(implementation = TokenDTO.class))),
+            @ApiResponse(responseCode = "200", description = "리프레시 성공"),
             @ApiResponse(responseCode = "401", description = "인증 실패", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class))),
-            @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content)
+            @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
     })
     @PostMapping("/refresh")
     public ResponseEntity<TokenDTO> refresh(@RequestBody TokenDTO tokenDTO) {
@@ -65,7 +83,7 @@ public class MemberController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "로그아웃 성공"),
             @ApiResponse(responseCode = "401", description = "인증 실패", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class))),
-            @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content)
+            @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
     })
     @PostMapping("/logout")
     public ResponseEntity<String> logout(@RequestBody TokenDTO tokenDTO) {
