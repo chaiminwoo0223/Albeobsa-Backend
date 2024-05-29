516903be546ff4decfa939e8fde5c3c2c2bf9015
최민우 <chaiminwoo0223@naver.com>
2024-05-30 00:16:54 +0900

    [#16] SecurityConfig 코드 최적화
    1.filterChain 메소드 수정
    2.googleClientRegistration 메소드 삭제
    3.clientRegistrationRepository 메소드 삭제
    4.authorizedClientService 메소드 삭제

--- src/main/java/skhu/jijijig/config/SecurityConfig.java
+++ src/main/java/skhu/jijijig/config/SecurityConfig.java
@@ -9,17 +9,10 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -46,29 +39,6 @@ public class SecurityConfig {
             "/swagger-ui/**"
     };
 
-    private ClientRegistration googleClientRegistration() {
-        return ClientRegistration.withRegistrationId("google")
-                .clientId("${spring.security.oauth2.google-client-id}")
-                .clientSecret("${spring.security.oauth2.google-client-secret}")
-                .redirectUri("${spring.security.oauth2.google-redirect-uri}")
-                .tokenUri("${spring.security.oauth2.google-token-url}")
-                .userInfoUri("${spring.security.oauth2.google-userInfo-uri}")
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .authorizationUri("https://accounts.google.com/o/oauth2/v2/auth")
-                .scope("https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email")
-                .build();
-    }
-
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        return new InMemoryClientRegistrationRepository(googleClientRegistration());
-    }
-
-    @Bean
-    public OAuth2AuthorizedClientService authorizedClientService(ClientRegistrationRepository clientRegistrationRepository) {
-        return new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository);
-    }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -76,10 +46,6 @@ public class SecurityConfig {
                 .csrf(CsrfConfigurer::disable) // CSRF 보호를 비활성화합니다.
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션을 사용하지 않습니다.
-                .oauth2Login(oauth2 -> oauth2
-                        .clientRegistrationRepository(clientRegistrationRepository()) // ClientRegistrationRepository를 설정합니다.
-                        .authorizedClientService(authorizedClientService(clientRegistrationRepository()))) // AuthorizedClientService를 설정합니다.
-                .logout(LogoutConfigurer::permitAll) // 로그아웃을 허용합니다.
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(PERMITTED_URLS).permitAll() // 특정 경로에 대한 접근을 모두 허용합니다.
                         .anyRequest().authenticated()) // 그 외 요청은 인증이 필요합니다.
