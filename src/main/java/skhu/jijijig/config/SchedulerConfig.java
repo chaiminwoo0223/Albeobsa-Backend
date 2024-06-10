7686ff62134a4d2331fe1bcbbe086cf2ac08706b
최민우 <chaiminwoo0223@naver.com>
2024-06-10 11:58:45 +0900

    [#23] docker-compose.yaml 수정 6

--- src/main/java/skhu/jijijig/config/SchedulerConfig.java
+++ src/main/java/skhu/jijijig/config/SchedulerConfig.java
@@ -10,7 +10,7 @@ public class SchedulerConfig {
     @Bean
     public ThreadPoolTaskScheduler taskScheduler() {
         ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
-        scheduler.setPoolSize(20);  // 스레드 풀 크기 설정
+        scheduler.setPoolSize(20);
         scheduler.setThreadNamePrefix("scheduled-task-");
         scheduler.initialize();
         return scheduler;
